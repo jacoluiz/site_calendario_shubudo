2f2e8d407732e933f5657c3ce0f257d7c703c22b
jacoluiz <30814790+jacoluiz@users.noreply.github.com>
2025-07-11 16:10:17 -0300

    Corrigir erro 404 com proxy correto

--- vite.config.ts
+++ vite.config.ts
@@ -9,18 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'http://3.17.71.237:3000',
         changeOrigin: true,
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   },
