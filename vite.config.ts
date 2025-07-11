3f25f834c060d4db27dacb271a26ec9da9da27aa
Jaco Luiz Chandeski <jacochandeski@gmail.com>
2025-07-11 18:12:04 -0300

    ja n sei mais

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/site_calendario_shubudo/', // <-- necessário para GitHub Pages
+  base: '/site_calendario_shubudo/', // necessário para GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
