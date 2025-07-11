32c6c178f5fc9c558bb545b2c089953ad1e78461
jacoluiz <30814790+jacoluiz@users.noreply.github.com>
2025-07-11 16:14:51 -0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -24,8 +24,8 @@ export default defineConfig({
             console.log(`Received proxy response: ${proxyRes.statusCode} for ${req.url}`);
           });
         },
-      }
-    }
+      },
+    },
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
